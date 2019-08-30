8c7d9185f191943e886c7d8be95e317624ea75d4
Tom Rothe <motine@users.noreply.github.com>
2019-08-30 10:54:42 +0200

    Update us-german-layout.rb

--- us-german-layout.rb
+++ us-german-layout.rb
@@ -4,7 +4,7 @@ class UsGermanLayout < Formula
   homepage "http://hci.rwth-aachen.de/USGermanKeyboard"
   url "http://hci.rwth-aachen.de/tiki-download_wiki_attachment.php?attId=793"
   version "0.98"
-  sha1 "949c856dee7ef0268457c412bc66a1f48f86b261"
+  # sha1 "949c856dee7ef0268457c412bc66a1f48f86b261"
 
   def install
     bundle_path = "USGerman Keyboard Layout 0.98/Roman.bundle/Contents/Resources"
